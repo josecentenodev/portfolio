3745843956493117a78b3a4077b1c4b9a4e30519
Centeno Jose Antonio <jocecito182@gmail.com>
2025-03-06 07:37:02 -0300

    Update experience section with expanded professional details and more comprehensive job descriptions

--- app/page.tsx
+++ app/page.tsx
@@ -6,20 +6,37 @@ import Experience from "./_components/Experience";
 
 const experienceData = [
   {
+    title: "Desarrollador Full Stack",
+    period: "Abril 2024 - Actualidad",
+    activities: [
+      "Desde el año pasado hasta hoy estoy trabajando en distintos proyectos para Ypsilon, una empresa de desarrollo de software en Córdoba, Argentina",
+      "Principalmente trabajo con Next.js, NextAuth, TailwindCSS, TypeScript, tRPC, PostgreSQL, Prisma, Docker, AWS",
+      "Mejoré considerablemente la interfaz de uno de los proyectos de la empresa, que es una aplicación web de digitalización de documentos",
+      "Construí una aplicación web para la gestión de clientes con usuarios y permisos de acceso a distintas aplicaciones de la empresa, brindándole la posibilidad de tener control sobre los clientes y sus aplicaciones, también senté las bases monitorear el uso de las aplicaciones",
+      "Éste año 2025 comencé con un gran desafío: el desarrollo de una aplicación web para el gobierno de la provincia de Córdoba, Argentina, que es una aplicación web para la gestión de solicitudes de servicios de los ciudadanos",
+      "Tuve que adecuarme a un nuevo stack de tecnologías: Angular, Angular Material, ahora utilizo Angular CLI para crear components, services, modules, etc.",
+      "Mi enfoque principal es escribir CLEAN CODE, teniendo en cuenta también SOLID principles, DRY principles, KISS principle, etc.",
+      "Intento seguir los principios de CLEAN ARCHITECTURE, para que la aplicación sea fácil de mantener y escalable",
+    ],
+  },
+  {
     title: "Desarrollador Frontend",
     period: "enero 2024 - marzo 2024",
     activities: [
-      "Desarrollé una aplicación web de gestión de usuarios utilizando React con Vite, TypeScript y TailwindCSS para una organización ",
+      "Desarrollé una aplicación web de gestión de usuarios utilizando React con Vite, TypeScript y TailwindCSS para una organización sin fines de lucro: AWAQ ONGD - Madrid, España",
       "Implementé una interfaz de usuario intuitiva y fácil de usar",
-      "Integré Firebase para la autenticación y el almacenamiento de datos",
+      "Integré con la API hecha en Node.js para la gestión de usuarios",
+      "Desarrollé un sistema de autenticación con JWT y encriptación de contraseñas",
+      "Utilicé React Router para la navegación entre las distintas vistas de la aplicación tanto para el usuario como para el administrador",
+      "El diseño de la aplicación fue realizado con Figma, no tuve participación en el diseño pero me encargué de implementarlo",
     ],
   },
   {
     title: "Desarrollador Frontend",
     period: "2022 - 2023",
     activities: [
-      "Estuve a cargo del desarrollo e implementación de características clave en una aplicación inspirada en GLPI",
-      "Utilicé tecnologías de vanguardia como TypeScript, React, TailwindCSS, Material UI, Axios y Django REST framework",
+      "Estuve a cargo del desarrollo e implementación de características clave en un proyecto inspirado en GLPI",
+      "Utilicé TypeScript, React, TailwindCSS, Material UI, Axios y Django REST framework",
       "Aseguré experiencia de usuario fluida y funcionalidad sólida en toda la aplicación.",
       "Tuve compromiso con las mejores prácticas de desarrollo SOLID.",
       "Resolví de manera proactiva cada desafío técnico",
